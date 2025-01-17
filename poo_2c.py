d085694a54f970b712cbe90dbe8ea039c6b63d64
VecinoLiliana <vecinocollililianamarisol@gmail.com>
2025-01-17 13:29:33 -0600

    Matadero

--- poo_2c.py
+++ poo_2c.py
@@ -32,13 +32,14 @@ class Personaje:
         print (self.nombre, "ha muerto")
 
     def dañar(self, enemigo):
-        return self.fuerza - enemigo.defensa
+        return max(0,self.fuerza - enemigo.defensa)
     
     def atacar (self, enemigo):
         daño = self.dañar(enemigo)
-        enemigo.vida = enemigo.vida - daño
+        enemigo.vida = max(0,enemigo.vida - daño)
         print (self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
         print ("Vida de", enemigo.nombre, "es", enemigo.vida)
+
 class Guerrero(Personaje):
     
     #Sobreescribir el constructor
@@ -46,10 +47,79 @@ class Guerrero(Personaje):
         super().__init__(nombre, fuerza, inteligencia, defensa, vida)
         self.espada = espada
 
-arturoSuarez = Guerrero( "Arturo Suárez", 12, 3000, 2, 100,.5)
-arturoSuarez.imprimir_atributos()
-print("El valor de espada es:", arturoSuarez.espada)
+    #Sobreescribir impresión de datos
+    def imprimir_atributos(self):
+        super().imprimir_atributos()
+        print("-Espada:", self.espada)
 
+    #Sobreescribir el cálculo del daño
+    def dañar(self, enemigo):
+        return self.fuerza*self.espada - enemigo.defensa
+    
+    #Escoger navaja
+    def escoger_navaja (self):
+        opcion = int (input ("Escoge la navaja de la muerte:\n (1)Navaja suiza.... 10 \n (2)Navaja pioja.... 03\n >>>>>>>>>>>"))
+        if (opcion == 1):
+            self.espada = 10
+        elif (opcion == 2):
+            self.espada = 6
+        else:
+            print ("Valor inválido, intente de nuevo")
+            #Vuelva a ejecutar el método escoger_navaja
+            self.escoger_navaja()
+
+        
+class Mago(Personaje):
+    
+    #Sobreescribir el constructor
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.libro = libro
+
+    #Sobreescribir impresión de datos
+    def imprimir_atributos(self):
+        super().imprimir_atributos()
+        print("-Libro:", self.libro)
+
+    #Sobreescribir el cálculo del daño
+    def dañar(self, enemigo):
+        return self.inteligencia*self.libro - enemigo.defensa
+    
+    #Escoger libro
+    def escoger_libro (self):
+        opcion = int (input ("Escoge el libro de la sabiduría:\n (1)El principito.... 10 \n (2)Crepúsculo.... 03\n >>>>>>>>>>>"))
+        if (opcion == 1):
+            self.libro = 10
+        elif (opcion == 2):
+            self.libro = 6
+        else:
+            print ("Valor inválido, intente de nuevo")
+            #Vuelva a ejecutar el método escoger_navaja
+            self.escoger_libro()
+
+#crear todo los objetos
+persona = Personaje("Angel Suárez", 20,15,10,100)
+arturoSuarez = Guerrero( "Arturo Suárez", 20, 15, 10, 100,5)
+gandalf = Mago("Gandalf",20, 15, 10, 100, 5)
+
+#atributos antes de la tragedia
+persona.imprimir_atributos()
+arturoSuarez.imprimir_atributos()
+gandalf.imprimir_atributos()
+
+#ataques sin piedad
+persona.atacar(arturoSuarez)
+arturoSuarez.atacar(gandalf)
+gandalf.atacar(persona)
+
+#Atributos despues de la tragedia 
+persona.imprimir_atributos()
+arturoSuarez.imprimir_atributos()
+gandalf.imprimir_atributos()
+
+
+#arturoSuarez.escoger_navaja()
+#print("El valor de espada es:", arturoSuarez.espada)
 
 # Variable del constructor
 # mi_personaje = Personaje ("EstebanDido", 100, 50, 45, 100)
